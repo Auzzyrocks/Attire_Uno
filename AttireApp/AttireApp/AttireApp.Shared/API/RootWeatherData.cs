fa0e3046ad980771af313c7b95e9c951a0064b2e
Brandon Cosh <655065746@viu.ca>
2022-11-18 00:46:57 -0800

    Added API files/classes

---
+++ AttireApp/AttireApp/AttireApp.Shared/API/RootWeatherData.cs
@@ -0,0 +1,13 @@
+﻿/*
+ * Author: Brandon Cosh
+ * Date: November 7, 2022
+ */
+
+namespace AttireApp.API
+{
+    class RootWeatherData
+    {
+        public RootForecast RootForecast { get; set; }
+        public RootCurrent RootCurrent { get; set; }
+    }
+}
---
+++ AttireApp/AttireApp/AttireApp.Shared/API/WeatherHandlerAPI.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using ABI.System;
+using System.Net.Http.Json;
+using System.Collections.Specialized;
+
+namespace AttireApp.API
+{
+    class WeatherHandlerAPI
+    {
+        public static HttpClient ApiClient;
+        public static void InitializeClient()
+        {
+            ApiClient = new HttpClient();
+            ApiClient.DefaultRequestHeaders.Accept.Clear();
+            ApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public static async Task<RootForecast> CallWeatherForecastAPI()
+        {
+            string location = "nanaimo";
+            string url = $"https://api.weatherapi.com/v1/current.json?key=0fa9f3b714ea42f8a2a235335222910&q={ location }&aqi=yes";
+            using (HttpResponseMessage response = await ApiClient.GetAsync(url))
+            {
+
+                if (response.IsSuccessStatusCode)
+                {
+                    RootForecast result = await response.Content.ReadFromJsonAsync<RootForecast>();
+                    return result;
+                }
+                else
+                {
+                    Console.WriteLine("Could not reach API: " + response.ReasonPhrase);
+                    return null;
+                }
+            }
+        }
+    }
+}
